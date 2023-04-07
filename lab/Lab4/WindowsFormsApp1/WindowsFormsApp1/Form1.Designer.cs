bd3ec49ca41cb7dd2dc5dfa8f2172e4f9b5ad8d8
IIvannM <93133422+IIvannM@users.noreply.github.com>
2023-04-07 10:58:25 +0300

    commit

---
+++ lab/Lab4/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ColorMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.PrintMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.Fontexm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.XtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.slagtxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.res = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 68);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ColorMenu,
+            this.FontMenu,
+            this.PrintMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ColorMenu
+            // 
+            this.ColorMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ColorMenu.Name = "ColorMenu";
+            this.ColorMenu.Size = new System.Drawing.Size(48, 20);
+            this.ColorMenu.Text = "Color";
+            this.ColorMenu.Click += new System.EventHandler(this.ColorMenu_Click);
+            // 
+            // FontMenu
+            // 
+            this.FontMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FontMenu.Name = "FontMenu";
+            this.FontMenu.Size = new System.Drawing.Size(43, 20);
+            this.FontMenu.Text = "Font";
+            this.FontMenu.Click += new System.EventHandler(this.FontMenu_Click);
+            // 
+            // PrintMenu
+            // 
+            this.PrintMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PrintMenu.Name = "PrintMenu";
+            this.PrintMenu.Size = new System.Drawing.Size(44, 20);
+            this.PrintMenu.Text = "Print";
+            this.PrintMenu.Click += new System.EventHandler(this.PrintMenu_Click);
+            // 
+            // Fontexm
+            // 
+            this.Fontexm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Fontexm.Location = new System.Drawing.Point(374, 44);
+            this.Fontexm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Fontexm.Multiline = true;
+            this.Fontexm.Name = "Fontexm";
+            this.Fontexm.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Fontexm.Size = new System.Drawing.Size(366, 100);
+            this.Fontexm.TabIndex = 2;
+            this.Fontexm.Text = "Text example";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(28, 205);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x =";
+            // 
+            // XtextBox
+            // 
+            this.XtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.XtextBox.Location = new System.Drawing.Point(65, 202);
+            this.XtextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.XtextBox.Name = "XtextBox";
+            this.XtextBox.Size = new System.Drawing.Size(80, 28);
+            this.XtextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(28, 272);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во слагаемых";
+            // 
+            // slagtxt
+            // 
+            this.slagtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.slagtxt.Location = new System.Drawing.Point(174, 272);
+            this.slagtxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.slagtxt.Name = "slagtxt";
+            this.slagtxt.Size = new System.Drawing.Size(73, 28);
+            this.slagtxt.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(498, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Результат";
+            // 
+            // res
+            // 
+            this.res.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res.Location = new System.Drawing.Point(442, 249);
+            this.res.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.res.Multiline = true;
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(237, 62);
+            this.res.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(275, 387);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 54);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(752, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.slagtxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.XtextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Fontexm);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ColorMenu;
+        private System.Windows.Forms.ToolStripMenuItem FontMenu;
+        private System.Windows.Forms.TextBox Fontexm;
+        private System.Windows.Forms.ToolStripMenuItem PrintMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox XtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox slagtxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox res;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
